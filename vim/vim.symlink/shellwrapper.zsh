61871e6e8c1baf6904e856c4ea3ee7aee5933823
Thomas Burk <t.burk@bckx.de>
2020-06-22 13:24:24 +0200

    Fix typo in comment

--- vim/vim.symlink/shellwrapper.zsh
+++ vim/vim.symlink/shellwrapper.zsh
@@ -9,7 +9,7 @@ clear
 # strip the -c that vim adds to bash from arguments to this script
 shift
 
-# echo a nice blue header line because it's prettier this way :-)
+# echo a nice header line because it's prettier this way :-)
 echo -e "\e[33;1m\n== Command Output ==\n\e[m"
 
 # run the command:
