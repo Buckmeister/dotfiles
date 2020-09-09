708d6512d6f129dbe8e40e8567b7b800a74fb092
Thomas Burk <t.burk@bckx.de>
2020-09-09 11:47:46 +0200

    Refactor based on shellcheck recommendations

--- brew/generate_brew_install_script.zsh
+++ brew/generate_brew_install_script.zsh
@@ -1,14 +1,14 @@
 #!/usr/bin/env zsh
 
-local script=$0
-if [ -L  $script ]; then
-  script=$(readlink $script)
+script=$0
+if [ -L  "${script}" ]; then
+  script=$(readlink "${script}")
 fi
 
-local scriptPath=$(dirname $script)
+scriptPath=$(dirname "${script}")
 echo "Output directory: ${scriptPath}"
 
-local outputScript=${scriptPath}/install_packages.zsh
+outputScript=${scriptPath}/install_packages.zsh
 echo "Output script: ${outputScript}"
 
 command -v brew > /dev/null 2>&1 || {
@@ -16,7 +16,7 @@ command -v brew > /dev/null 2>&1 || {
   exit 1
 }
 
-cat << END_OF_HEADER > $outputScript
+cat << END_OF_HEADER > "${outputScript}"
 #!/usr/bin/env zsh
 
 command -v brew > /dev/null 2>&1 || {
@@ -33,8 +33,8 @@ command -v brew > /dev/null 2>&1 || {
 
 END_OF_HEADER
 
-brew cask list | awk '{print "brew cask install "$0}' >> $outputScript
-brew leaves | awk '{print "brew install "$0}' >> $outputScript
+brew cask list | awk '{print "brew cask install "$0}' >> "${outputScript}"
+brew leaves | awk '{print "brew install "$0}' >> "${outputScript}"
 
 "${scriptPath}/post_generate.zsh"
 
